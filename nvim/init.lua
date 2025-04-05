6abf7e2cfa2ad805bfca3461a0d4bc2df8e0b232
prashant-2204 <prashantair10@gmail.com>
2025-04-05 10:31:19 +0530

    added readme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require 'core.options' -- Load general options
 require 'core.keymaps' -- Load general keymaps
 require 'core.snippets' -- Custom code snippets
-
+--luainit
 -- Install package manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
